58442ce48fb2d9ca8e195cb8a246aafa873677a3
Sasikeshwaran <98255438+Sasikeshwaran@users.noreply.github.com>
2025-03-21 18:08:46 +0530

    initial commit

---
+++ Transformation/src/com/Runner/Testing.java
@@ -0,0 +1,19 @@
+package com.Runner;
+
+public class Testing {
+	
+	public void Govert() {
+		System.out.println("Tamil Government was declare the new budget");
+	}
+	
+	public void Govern() {
+		System.out.println("TamiNadu Government is ceheating a people in this budget");
+	}
+   
+	public static void main (String[] arg) {
+		Testing t = new Testing();
+		t.Govert();
+		t.Govern();
+				
+	}
+}
